fc11f002f88e947dfaf2bc4e06dc634f0df10477
Everest Oreizy <everest@oreizy.com>
2021-11-13 10:41:46 -0800

    wheeltestt fixes??

--- TeamCode/src/main/java/org/eps/WheelTest.java
+++ TeamCode/src/main/java/org/eps/WheelTest.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name="WheelTest", group="9884")
 public class WheelTest extends LinearOpMode {
 
+    double factor = .4;
     //declare opmode members
     Hardware robot = new Hardware();
 
@@ -16,42 +17,46 @@ public class WheelTest extends LinearOpMode {
         robot.init(hardwareMap);
 
         //telemetry
-        telemetry.addData("Status", "started");
+        telemetry.addData("Status", "WithOpModeActive");
         telemetry.update();
 
         waitForStart();
-        robot.FrontRightMotor.setPower(1);
-        robot.threadSleep(5000);
-        robot.FrontRightMotor.setPower(-1);
-        robot.threadSleep(5000);
+//        while(opModeIsActive()) {
+            robot.FrontRightMotor.setPower(factor);
+            robot.threadSleep(5000);
+            robot.FrontRightMotor.setPower(-factor);
+            robot.threadSleep(5000);
+            robot.FrontRightMotor.setPower(0);
 
-        robot.FrontLeftMotor.setPower(1);
-        robot.threadSleep(5000);
-        robot.FrontLeftMotor.setPower(-1);
-        robot.threadSleep(5000);
+            robot.FrontLeftMotor.setPower(factor);
+            robot.threadSleep(5000);
+            robot.FrontLeftMotor.setPower(-factor);
+            robot.threadSleep(5000);
+            robot.FrontLeftMotor.setPower(0);
 
-        robot.BackRightMotor.setPower(1);
-        robot.threadSleep(5000);
-        robot.BackRightMotor.setPower(-1);
-        robot.threadSleep(5000);
+            robot.BackRightMotor.setPower(factor);
+            robot.threadSleep(5000);
+            robot.BackRightMotor.setPower(-factor);
+            robot.threadSleep(5000);
+            robot.BackRightMotor.setPower(0);
 
-        robot.BackLeftMotor.setPower(1);
-        robot.threadSleep(5000);
-        robot.BackLeftMotor.setPower(-1);
-        robot.threadSleep(5000);
+            robot.BackLeftMotor.setPower(factor);
+            robot.threadSleep(5000);
+            robot.BackLeftMotor.setPower(-factor);
+            robot.threadSleep(5000);
+            robot.BackLeftMotor.setPower(0);
 
-        robot.FrontRightMotor.setPower(1);
-        robot.FrontLeftMotor.setPower(1);
-        robot.BackRightMotor.setPower(1);
-        robot.BackLeftMotor.setPower(1);
-        robot.threadSleep(5000);
+            robot.powerAllOne(factor);
+            robot.threadSleep(5000);
 
-        robot.FrontRightMotor.setPower(-1);
-        robot.FrontLeftMotor.setPower(-1);
-        robot.BackRightMotor.setPower(-1);
-        robot.BackLeftMotor.setPower(-1);
-        robot.threadSleep(5000);
-        //stuff u want to do goes here
+            robot.powerAllOne(-factor);
+            robot.threadSleep(5000);
+
+            robot.powerAllOne(0);
+//            stop();
+//            break;
+            //stuff u want to do goes here
+//        }
     }
 
 }
